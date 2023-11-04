db9210ad2ad9486644d5708f4d165c2d246caa8d
carlosalvaro98 <128048300+carlosalvaro98@users.noreply.github.com>
2023-11-04 21:20:05 +0000

    Update Profile/EditProfile
    -funcionalidades principais já foram adicionadas e estão a funcionar corretamente.

--- app/src/main/java/com/example/onrequest/UserProfileMain.java
+++ app/src/main/java/com/example/onrequest/UserProfileMain.java
@@ -3,14 +3,17 @@ package com.example.onrequest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.onrequest.schema.dao.UserProfileDao;
 import com.example.onrequest.schema.db.AppDatabase;
+import com.example.onrequest.schema.entity.UserProfile;
 
 public class UserProfileMain extends AppCompatActivity {
 
@@ -46,5 +49,27 @@ public class UserProfileMain extends AppCompatActivity {
     public void openEditProfile(){
         Intent intent = new Intent(this, EditUserProfile.class);
         startActivity(intent);
+    }
+
+    public void updateProfileView(){
+        AppDatabase db = AppDatabase.getInstance(this);
+        UserProfileDao userProfileDao = db.getUserProfileDao();
+        UserProfile userProfile = userProfileDao.getUserProfile();
+
+        if (userProfile != null){
+            textViewName.setText(userProfile.getName());
+            textViewEmail.setText(userProfile.getMail());
+
+            String photoUri = userProfile.getPhoto();
+            if (photoUri != null && !photoUri.isEmpty()){
+                Glide.with(this).load(Uri.parse(photoUri)).into(imageViewProfilePhoto);
+            }
+        }
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        updateProfileView();
     }
 }
