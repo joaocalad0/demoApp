df60b1590a28205736e66ce1587847003afdd8f0
carlosalvaro98 <128048300+carlosalvaro98@users.noreply.github.com>
2023-12-13 20:54:11 +0000

    API fix
    correção da BASE_URL da api

--- app/src/main/java/com/example/onrequest/API/DataSource.java
+++ app/src/main/java/com/example/onrequest/API/DataSource.java
@@ -6,7 +6,9 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class DataSource {
 
     //private static final String BASE_URL = "http://127.0.0.1:8000/api/";
-    private static final String BASE_URL = "http://10.50.45.137:8000/api/";
+    //private static final String BASE_URL = "http://127.0.0.1:8000/api/";
+    private static final String BASE_URL = "http://10.0.2.2:8000/api/";
+    //private static final String BASE_URL = "http://10.50.45.137:8000/api/";
 
 
     private static Retrofit retrofit;
